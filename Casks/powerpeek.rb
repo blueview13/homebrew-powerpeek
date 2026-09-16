c75d775ee0f085a05b7a2db1a9eee991eeced841
Doug <dougbennett2@gmail.com>
2026-09-16 10:26:29 +0100

    Create Cask for PowerPeek version 0.1.0
    Add cask for PowerPeek with version and metadata

--- Casks/powerpeek.rb
+++ Casks/powerpeek.rb
@@ -1,1 +1,12 @@
+cask "powerpeek" do
+  version "0.1.0"
+  sha256 "b04841c378389ba885d3fb4f707a5350e37612c93c3c11dd427cda3ff8b6dfcd"
 
+  url "https://github.com/blueview13/powerpeek/releases/download/v#{version}/PowerPeek-#{version}.dmg"
+
+  name "PowerPeek"
+  desc "Clearer battery status display for macOS"
+  homepage "https://github.com/blueview13/powerpeek"
+
+  app "PowerPeek.app"
+end
